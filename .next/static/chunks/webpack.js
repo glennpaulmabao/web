af584c7adcdad85b10ab23c6bb15b92930f2b7ce
Glenn Paul Mabao <64250849+glennpaulmabao@users.noreply.github.com>
2023-05-30 10:25:33 +0800

    UPDATES!

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b3f233fc7c272975"; }
+/******/ 		__webpack_require__.h = function() { return "0dd045054648dd2e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/resume.js
+++ pages/resume.js
@@ -5,9 +5,11 @@ import thisresume from "../public/Glenn-Paul-Mabao-2023-Resume.jpg";
 
 function resume() {
     return (
-      <div className="bg-[#EFF5D5] min-h-full p-5 m-0">
-        <a href="../Glenn-Paul-Mabao-2023-Resume.pdf" download className="bg-[#D5FF3F] py-3 px-10 rounded-3xl mt-10 absolute" >Download a Copy</a>
-        <Image alt="Resume" width="auto" height="auto" src={thisresume} className="h-[100%] w-[100%] md:w-[70%] md:h-auto mx-auto rounded-xl" />
+      <div className="bg-[#EFF5D5] h-full p-5 m-0 grid grid-flow-row">
+        <Image alt="Resume" width="auto" height="auto" src={thisresume} className="min-h-[100%] w-[100%] md:w-[70%] md:h-auto mx-auto rounded-xl" />
+        <div className='w-[100%] flex mx-auto justify-center'>
+        <a href="../Glenn-Paul-Mabao-2023-Resume.pdf" download className="bg-[#D5FF3F] hover:bg-black hover:text-[#D5FF3F] py-3 px-10 rounded-3xl mt-10" >Download a Copy</a>
+        </div>
       </div>
     );
   }
