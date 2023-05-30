f7a6272697bd3bb8fe2b6a6e522d75977d3dc9e1
Glenn Paul Mabao <64250849+glennpaulmabao@users.noreply.github.com>
2023-05-30 11:12:55 +0800

    250px width added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "421da8523883d672"; }
+/******/ 		__webpack_require__.h = function() { return "9f3ae3c25879f7d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/resume.js
+++ pages/resume.js
@@ -7,9 +7,9 @@ function resume() {
     return (
       <div className="bg-[#EFF5D5] min-h-screen p-5 m-0 grid grid-flow-row">
         <Image alt="Resume" width="auto" height="auto" src={thisresume} className="min-h-auto w-auto md:w-[70%] md:h-auto mx-auto rounded-xl" />
-        <center className='m-20'>
+        <center className='flex w-full m-20'>
         <div className='w-full mx-auto'>
-        <a href="../Glenn-Paul-Mabao-2023-Resume.pdf" download className="bg-[#D5FF3F] hover:bg-black hover:text-[#D5FF3F] w-full  py-3 px-5 rounded-3xl" >Download a Copy</a>
+        <a href="../Glenn-Paul-Mabao-2023-Resume.pdf" download className="bg-[#D5FF3F] hover:bg-black hover:text-[#D5FF3F] w-[250px]  py-3 px-10 rounded-3xl" >Download a Copy</a>
         </div>
         </center>
       </div>
