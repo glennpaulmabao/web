0ae353c0f8c10c8618932c74e6d951f28fa9af84
Glenn Paul Mabao <64250849+glennpaulmabao@users.noreply.github.com>
2023-11-10 14:02:06 +0800

    the designer updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1e63bcfa8fd0ccfb"; }
+/******/ 		__webpack_require__.h = function() { return "89a4935d0ee44aa3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
